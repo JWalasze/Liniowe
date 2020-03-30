4851b59cf5c2b16acc9b519abed7a6ff927fb669
Jakub Walaszek <252897@student.pwr.edu.pl>
2020-03-30 20:10:30 +0200

    dodany zalazek

---
+++ zadanie/src/Macierz.cpp
@@ -0,0 +1,9 @@
+#include "../inc/Macierz.hh"
+
+
+/*
+ *  Tutaj nalezy zdefiniowac odpowiednie metody
+ *  klasy Macierz, ktore zawieraja wiecej kodu
+ *  niz dwie linijki.
+ *  Mniejsze metody mozna definiwac w ciele klasy.
+ */
---
+++ zadanie/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../inc/Macierz.hh"
+#include "../inc/UkladRownanLiniowych.hh"
+#include "../inc/Wektor.hh"
+
+
+
+using namespace std;
+
+/*
+ * Tu definiujemy pozostale funkcje.
+ * Lepiej jednak stworzyc dodatkowy modul
+ * i tam je umiescic. Ten przyklad pokazuje
+ * jedynie absolutne minimum.
+ */
+
+
+int main()
+{
+  UkladRownanLiniowych   UklRown;   // To tylko przykladowe definicje zmiennej
+
+
+  cout << endl << " Start programu " << endl << endl;
+}
